e1000cd062d8a7a8581c407fea6c64ad21c4acb6
karinacruz98 <57500704+karinacruz98@users.noreply.github.com>
2020-05-31 19:57:27 -0300

    Add files via upload

---
+++ js/filtra.js
@@ -0,0 +1,28 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function() {
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i"); //funcao para pegar o valor, nao case sensitive
+
+            if (!expressao.test(nome)) { //Validação de teste se na variavel nome, tem algum valor da variavel expressa
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+
+    
+});
